84fe1dc9dcff3f2d5b0b76ecd113ae01ae1c02ec
Vicky Varma <varmasvicky26@gmail.com>
2025-07-28 11:45:33 +0000

    Back to Game Options green color add karo is botton me

--- src/components/game-betting-layout.tsx
+++ src/components/game-betting-layout.tsx
@@ -26,7 +26,7 @@ export function GameBettingLayout({ gameName, gameId, activeBetType, children }:
         </div>
 
         <div className="my-6">
-            <Button asChild variant="outline" className="w-full">
+            <Button asChild variant="default" className="w-full bg-green-500 hover:bg-green-600 text-white">
                 <Link href={`/games/${gameId}`} className="flex items-center gap-2">
                     <ArrowLeft className="h-4 w-4"/>
                     <span>Back to Game Options</span>
