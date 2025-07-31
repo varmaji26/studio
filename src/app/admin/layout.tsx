7b4a48e391aa9d4abd9bbc91b23eafe2f262d1f6
Vicky Varma <varmasvicky26@gmail.com>
2025-07-31 14:23:42 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SidebarHeader>
    
    w

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -47,7 +47,7 @@ export default function AdminLayout({
             <div className="p-1.5 rounded-lg bg-primary">
                 <Trophy className="h-6 w-6 text-primary-foreground" />
             </div>
-            <h1 className="text-xl font-bold text-primary-foreground">Matka Genius</h1>
+            <h1 className="text-xl font-bold text-primary-foreground">Matka King</h1>
           </div>
       </SidebarHeader>
       <SidebarContent className="p-2">
