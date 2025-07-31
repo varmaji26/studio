1310037b1696f6ac8f3baff1d9940fdfcffddc1d
Vicky Varma <varmasvicky26@gmail.com>
2025-07-31 14:22:02 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="o

--- src/components/layout-provider.tsx
+++ src/components/layout-provider.tsx
@@ -113,7 +113,7 @@ export function LayoutProvider({
                 </div>
                 <div className="flex items-center gap-4">
                   <Link href="/">
-                    <Button variant="outline">
+                    <Button className="bg-green-500 text-white hover:bg-green-600">
                       <Home className="mr-2 h-4 w-4" />
                       Go to User Panel
                     </Button>
