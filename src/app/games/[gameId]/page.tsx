ff127f4e9ae0f3e96a0d032be1105ea41e06af75
Vicky Varma <varmasvicky26@gmail.com>
2025-08-05 08:44:02 +0000

    Yeh page se back jate hi mage ke pas back hojaye naki Quick Actions ke p

--- src/app/games/[gameId]/page.tsx
+++ src/app/games/[gameId]/page.tsx
@@ -98,13 +98,11 @@ export default function GamePage() {
         </div>
         
         <div className="my-6">
-            <Button asChild variant="default" className="w-full bg-green-500 hover:bg-green-600 text-white">
-                <Link href="/" scroll={false}>
-                    <div className="flex items-center gap-2">
-                        <ArrowLeft className="h-4 w-4"/>
-                        <span>Back to Home</span>
-                    </div>
-                </Link>
+            <Button variant="default" className="w-full bg-green-500 hover:bg-green-600 text-white" onClick={() => router.back()}>
+                <div className="flex items-center gap-2">
+                    <ArrowLeft className="h-4 w-4"/>
+                    <span>Back to Home</span>
+                </div>
             </Button>
         </div>
 
