525dc0e11d50d548472c6e99a6dcb27938f41c1d
Vicky Varma <varmasvicky26@gmail.com>
2025-08-05 12:44:40 +0000

    Nahi huva back to home karne ke baad back to game option vale page pe ja

--- src/components/game-betting-layout.tsx
+++ src/components/game-betting-layout.tsx
@@ -28,10 +28,10 @@ export function GameBettingLayout({ gameName, gameId, activeBetType, children }:
 
         <div className="my-6">
              <Button asChild variant="default" className="w-full bg-green-500 hover:bg-green-600 text-white">
-                <Link href={`/games/${gameId}`} scroll={false}>
+                <Link href="/" scroll={false}>
                     <div className="flex items-center gap-2">
                         <ArrowLeft className="h-4 w-4"/>
-                        <span>Back to Game Options</span>
+                        <span>Back to Home</span>
                     </div>
                 </Link>
             </Button>
