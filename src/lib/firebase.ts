a7537ba7f67c7365113a79f51e9d58149e3bbdab
Vicky Varma <varmasvicky26@gmail.com>
2025-08-06 18:56:47 +0000

    const firebaseConfig = {   apiKey: "AIzaSyDEZ0vQLJ2kY_3_pdWqZw7OjJVoQvoe

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDEZ0vQLJ2kY_3_pdWqZw7OjJVoQvoeJjM",
+  authDomain: "auth-canvas.firebaseapp.com",
+  projectId: "auth-canvas",
+  storageBucket: "auth-canvas.appspot.com",
+  messagingSenderId: "645923871999",
+  appId: "1:645923871999:web:848febd795743ae4e4c0ca"
 };
 
 // Initialize Firebase
