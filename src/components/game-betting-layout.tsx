a4ed975094c74275aa744df20221ce31a61e97d8
Vicky Varma <varmasvicky26@gmail.com>
2025-08-10 21:15:29 +0000

    Back to Gamen pe click karte hi Choose a Bet Type page pe ajaye

--- src/components/game-betting-layout.tsx
+++ src/components/game-betting-layout.tsx
@@ -28,7 +28,7 @@ export function GameBettingLayout({ gameName, gameId, activeBetType, children }:
 
         <div className="my-6">
              <Button asChild variant="default" className="w-full bg-green-500 hover:bg-green-600 text-white">
-                <Link href={`/#${gameId}`} scroll={false}>
+                <Link href={`/games/${gameId}`}>
                     <div className="flex items-center gap-2">
                         <ArrowLeft className="h-4 w-4"/>
                         <span>Back to Game</span>
