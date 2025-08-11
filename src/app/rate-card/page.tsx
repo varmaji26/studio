cc14dff67434e67fe3591ce26cf229bd9ac18f0e
Vicky Varma <varmasvicky26@gmail.com>
2025-08-11 20:58:52 +0000

    Aur chota karo size

--- src/app/rate-card/page.tsx
+++ src/app/rate-card/page.tsx
@@ -22,8 +22,8 @@ const standardGameRates = [
 
 const RateItem = ({ name, rate }: { name: string; rate: string }) => (
     <div className="bg-indigo-600 text-white flex justify-between items-center p-3 rounded-lg shadow-md">
-        <span className="font-semibold text-base">{name}</span>
-        <span className="font-bold text-base">{rate}</span>
+        <span className="font-semibold text-sm">{name}</span>
+        <span className="font-bold text-sm">{rate}</span>
     </div>
 );
 
