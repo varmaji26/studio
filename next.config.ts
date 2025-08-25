3376115be1e096c9e0f062013089ea50f52c8f91
Vicky Varma <varmasvicky26@gmail.com>
2025-08-25 15:47:46 +0000

    Mujhe kitni bar ye bolna padega aap kyon nahi samjh rahe publish nahi ho

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
