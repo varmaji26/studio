098cac6ce8b135185f5cdfdab3866b4fb707e283
Vicky Varma <varmasvicky26@gmail.com>
2025-09-02 13:21:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -19,16 +19,6 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.next_public_firebase_api_key,
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.next_public_firebase_auth_domain,
-    NEXT_PUBLIC_FIREBASE_DATABASE_URL: process.env.next_public_firebase_database_url,
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.next_public_firebase_project_id,
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.next_public_firebase_storage_bucket,
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.next_public_firebase_messaging_sender_id,
-    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.next_public_firebase_app_id,
-    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: process.env.next_public_firebase_measurement_id,
-  },
 };
 
 module.exports = nextConfig;
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,14 +5,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.next_public_firebase_api_key,
+  authDomain: process.env.next_public_firebase_auth_domain,
+  databaseURL: process.env.next_public_firebase_database_url,
+  projectId: process.env.next_public_firebase_project_id,
+  storageBucket: process.env.next_public_firebase_storage_bucket,
+  messagingSenderId: process.env.next_public_firebase_messaging_sender_id,
+  appId: process.env.next_public_firebase_app_id,
+  measurementId: process.env.next_public_firebase_measurement_id,
 };
 
 // Initialize Firebase
