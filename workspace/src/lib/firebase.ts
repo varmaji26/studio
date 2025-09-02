9e5c149b3383554aa733db7582456599a206e02d
Vicky Varma <varmasvicky26@gmail.com>
2025-09-02 19:15:31 +0000

    जैसे, आपकी firebase.ts फ़ाइल में कोड कुछ ऐसा दिखना चाहिए:
    TypeScript
    
    /

--- workspace/src/lib/firebase.ts
+++ workspace/src/lib/firebase.ts
@@ -1,22 +1,13 @@
-import { getStorage } from 'firebase/storage';
+// import { FirebaseOptions } from 'firebase/app';
+// import { getStorage } from 'firebase/storage';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL || "",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
-  apiKey: "AIzaSyCTncE_u2wUR8W3ptwlRuDG4wmCjI6bF-w",
-  authDomain: "matka-king-66ec3.firebaseapp.com",
-  databaseURL: "https://matka-king-66ec3-default-rtdb.firebaseio.com",
-  projectId: "matka-king-66ec3",
-  storageBucket: "matka-king-66ec3.appspot.com",
-  messagingSenderId: "358988541311",
-  appId: "1:358988541311:web:03491fbc1031220e16be4a",
-  measurementId: "G-GFZW681BYB"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-
-// Initialize Firebase
