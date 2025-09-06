3981fb1fd0a790146a549fe82eb5dbd7b91c7f3f
Vicky Varma <varmasvicky26@gmail.com>
2025-09-06 12:04:01 +0000

    https://auth-canvas.web.app/ yelink pe kaise deploy kare

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getStorage } from 'firebase/storage';
 import { getMessaging, isSupported } from 'firebase/messaging';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCTncE_u2wUR8W3ptwlRuDG4wmCjI6bF-w",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "auth-canvas.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "auth-canvas",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "auth-canvas.appspot.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "358988541311",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:358988541311:web:03491fbc1031220e16be4a",
 };
 
 // Initialize Firebase
