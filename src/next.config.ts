4cd9917f8e4690b35f63cfc950a7ef9991460f85
Vicky Varma <varmasvicky26@gmail.com>
2025-09-06 12:22:00 +0000

    आपने अपने Next.js एप्लिकेशन को Firebase पर सफलतापूर्वक डिप्लॉय कर दिया ह

---
+++ src/next.config.ts
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
+
+module.exports = nextConfig;
