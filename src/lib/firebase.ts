d177c1c48e7d4b53d7c808b77f8fd58beaa2f2e4
Vicky Varma <varmasvicky26@gmail.com>
2025-09-07 20:53:57 +0000

    const firebaseConfig = {   apiKey: "AIzaSyCTncE_u2wUR8W3ptwlRuDG4wmCjI6b

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,11 +7,13 @@ import { getMessaging, isSupported } from 'firebase/messaging';
 
 const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCTncE_u2wUR8W3ptwlRuDG4wmCjI6bF-w",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "auth-canvas.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "auth-canvas",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "auth-canvas.appspot.com",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "matka-king-66ec3.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "matka-king-66ec3",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "matka-king-66ec3.appspot.com",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "358988541311",
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:358988541311:web:03491fbc1031220e16be4a",
+  databaseURL: "https://matka-king-66ec3-default-rtdb.firebaseio.com",
+  measurementId: "G-GFZW681BYB"
 };
 
 // Initialize Firebase
