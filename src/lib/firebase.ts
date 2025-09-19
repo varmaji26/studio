586fb1bcb99d10413f0e0dad6e8fcedbf0042044
Vicky Varma <varmasvicky26@gmail.com>
2025-09-19 09:53:11 +0000

    apiKey: "AIzaSyCTncE_u2wUR8W3ptwlRuDG4wmCjI6bF-w",   authDomain: "matka-

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getStorage } from 'firebase/storage';
 import { getMessaging, isSupported } from 'firebase/messaging';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCTncE_u2wUR8W3ptwlRuDG4wmCjI6bF-w",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "matka-king-66ec3.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "matka-king-66ec3",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "matka-king-66ec3.appspot.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "358988541311",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:358988541311:web:03491fbc1031220e16be4a",
+  apiKey: "AIzaSyCTncE_u2wUR8W3ptwlRuDG4wmCjI6bF-w",
+  authDomain: "matka-king-66ec3.firebaseapp.com",
+  projectId: "matka-king-66ec3",
+  storageBucket: "matka-king-66ec3.firebasestorage.app",
+  messagingSenderId: "358988541311",
+  appId: "1:358988541311:web:03491fbc1031220e16be4a",
   databaseURL: "https://matka-king-66ec3-default-rtdb.firebaseio.com",
   measurementId: "G-GFZW681BYB"
 };
