65d42632f1b1329cc41c0268fc9d12089061cb67
Vicky Varma <varmasvicky26@gmail.com>
2025-09-22 13:45:47 +0000

    <script src="https://unpkg.com/@lottiefiles/dotlottie-wc@0.8.1/dist/dotl

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,20 +1,25 @@
 import { cn } from '@/lib/utils';
 
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'dotlottie-wc': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+        src: string;
+        autoplay?: boolean;
+        loop?: boolean;
+      }, HTMLElement>;
+    }
+  }
+}
+
 export function Loader({ className }: { className?: string }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={cn('animate-spin', className)}
-    >
-      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-    </svg>
+    <div className={cn(className)}>
+        <dotlottie-wc 
+            src="https://lottie.host/0df14788-d445-40bb-b7e6-413e847a169e/x9kAYkhru6.lottie"
+            autoplay 
+            loop>
+        </dotlottie-wc>
+    </div>
   );
 }
