288a07b6cc788783e88c7ea6f31b5d90042003a7
Vicky Varma <varmasvicky26@gmail.com>
2025-09-25 06:21:59 +0000

    ye loader ko change karo kuch achha sa

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,26 +1,9 @@
 import { cn } from '@/lib/utils';
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'dotlottie-wc': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
-        src: string;
-        autoplay?: boolean;
-        loop?: boolean;
-      }, HTMLElement>;
-    }
-  }
-}
-
 export function Loader({ className }: { className?: string }) {
   return (
     <div className={cn("flex justify-center items-center", className)}>
-        <dotlottie-wc 
-            src="https://lottie.host/0df14788-d445-40bb-b7e6-413e847a169e/x9kAYkhru6.lottie"
-            autoplay 
-            loop
-            style={{ width: '500px', height: '500px' }}>
-        </dotlottie-wc>
+        <span className="loader"></span>
     </div>
   );
 }
