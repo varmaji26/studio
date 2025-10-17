57c5877f91652065ffec499e84e3a67b529090bc
Vicky Varma <varmasvicky26@gmail.com>
2025-10-17 20:33:03 +0000

    loader ko change karo

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -3,7 +3,11 @@ import { cn } from '@/lib/utils';
 export function Loader({ className }: { className?: string }) {
   return (
     <div className={cn("flex justify-center items-center", className)}>
-        <span className="loader"></span>
+        <div className="loader">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
     </div>
   );
 }
