e4ebcbbf1be2d54c4ccaa4ec9f8c49017f61a46e
Vicky Varma <varmasvicky26@gmail.com>
2025-10-17 20:37:40 +0000

    animation dikh nahi raha hai isme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -184,6 +184,15 @@ export default function Home() {
   const currentDay = useMemo(() => new Date().toLocaleString('en-US', { weekday: 'long' }), []);
 
   useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const script = document.createElement('script');
+      script.src = 'https://unpkg.com/@lottiefiles/dotlottie-wc@latest/dist/dotlottie-wc.js';
+      script.type = 'module';
+      document.body.appendChild(script);
+    }
+  }, []);
+
+  useEffect(() => {
     if (!loading && !user) {
       router.replace('/login');
     }
@@ -530,11 +539,11 @@ export default function Home() {
             <CardContent className="p-0 relative h-64 flex flex-col items-center justify-center bg-black">
                 <div className="absolute inset-0">
                     <dotlottie-wc
-                        src="https://lottie.host/4f977458-1405-4f40-8730-17e1139a04a3/h2A9i32B51.json"
+                        src="https://lottie.host/e31a3138-1558-4171-87a4-37059119747d/uIclL8hG4a.json"
                         autoplay
                         loop
                         style={{ width: '100%', height: '100%' }}
-                    />
+                    ></dotlottie-wc>
                 </div>
                 <div 
                     className="relative z-10 text-center text-white"
@@ -630,5 +639,7 @@ export default function Home() {
     </div>
   );
 }
+
+    
 
     
