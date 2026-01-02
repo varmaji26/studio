b0dc6639f37cfcd4ee0ab9d9381d3ad85782930c
Vicky Varma <varmasvicky26@gmail.com>
2026-01-02 12:17:58 +0000

    Try fixing this error: `Console Error: FirebaseError: Messaging: A probl

--- src/lib/firebase-messaging.ts
+++ src/lib/firebase-messaging.ts
@@ -19,7 +19,6 @@ export const requestForToken = async (userId: string) => {
       console.log('Notification permission granted.');
       // Get the token
       const currentToken = await getToken(messaging, { 
-          vapidKey: 'BM-2iy1G5_2mnN8A5II7mf0pTISah5HU7i9xQx6hB_8-Fk2y9L3V-w4y_C0X_5d0AFlY50D_c5lZ_619WTJ-Q_Y',
           serviceWorkerRegistration: await navigator.serviceWorker.ready
       });
       if (currentToken) {
