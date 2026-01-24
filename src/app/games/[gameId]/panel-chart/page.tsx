38735683142669f60b22d6183564e36f68eead42
Vicky Varma <varmasvicky26@gmail.com>
2026-01-24 21:04:29 +0000

    jaha aisa red yani cut number red hote waha ye sab red hona chahiye 235

--- src/app/games/[gameId]/panel-chart/page.tsx
+++ src/app/games/[gameId]/panel-chart/page.tsx
@@ -41,12 +41,15 @@ const DayCell = ({ dayData }: { dayData: { openPana: string; jodi: string; close
     const isRed = isRedNumber(jodi);
     
     return (
-        <div className="flex items-center justify-center p-0 min-h-[30px] gap-0">
-            <div className="text-center text-[9px] text-black font-semibold leading-tight flex flex-col">
+        <div className={cn(
+            "flex items-center justify-center p-0 min-h-[30px] gap-0",
+            isRed ? 'text-red-600' : 'text-black'
+        )}>
+            <div className="text-center text-[9px] font-semibold leading-tight flex flex-col">
                 {openPana.split('').map((digit, i) => <span key={i}>{digit === '*' ? ' ' : digit}</span>)}
             </div>
-            <span className={cn("text-sm mx-0.5 font-bold", isRed ? 'text-red-600' : 'text-black')}>{jodi}</span>
-            <div className="text-center text-[9px] text-black font-semibold leading-tight flex flex-col">
+            <span className="text-sm mx-0.5 font-bold">{jodi}</span>
+            <div className="text-center text-[9px] font-semibold leading-tight flex flex-col">
                 {closePana.split('').map((digit, i) => <span key={i}>{digit === '*' ? ' ' : digit}</span>)}
             </div>
         </div>
