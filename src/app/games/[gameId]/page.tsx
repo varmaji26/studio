fe1e399d11a017f191fd687cc31edd1c427dde7e
Vicky Varma <varmasvicky26@gmail.com>
2026-01-24 19:42:46 +0000

    jaise maine SUPREME NIGHT game play kiya aue bet lagaya ab main game se

--- src/app/games/[gameId]/page.tsx
+++ src/app/games/[gameId]/page.tsx
@@ -215,7 +215,7 @@ export default function GamePage() {
         </div>
         
         <div className="my-2">
-            <Button variant="default" className="w-full bg-green-500 hover:bg-green-600 text-white h-9" onClick={() => router.back()}>
+            <Button variant="default" className="w-full bg-green-500 hover:bg-green-600 text-white h-9" onClick={() => router.push(`/#${gameId}`)}>
                 <div className="flex items-center gap-2">
                     <ArrowLeft className="h-4 w-4"/>
                     <span className="text-sm">Back to Home</span>
