e5cbafa217fe9111e351b675724fd6ba86dabc35
Vicky Varma <varmasvicky26@gmail.com>
2026-01-24 20:04:09 +0000

    shi to hogaya hai lekin mere dost ye srif SRIDEVI NIGHT pe hi back ara h

--- src/components/game-betting-layout.tsx
+++ src/components/game-betting-layout.tsx
@@ -25,10 +25,10 @@ export function GameBettingLayout({ gameName, gameId, activeBetType, children }:
         </div>
 
         <div className="my-2">
-             <Button variant="default" className="w-full bg-green-500 hover:bg-green-600 text-white h-9" onClick={() => router.back()}>
+             <Button variant="default" className="w-full bg-green-500 hover:bg-green-600 text-white h-9" onClick={() => router.push(`/#${gameId}`)}>
                 <div className="flex items-center gap-2">
                     <ArrowLeft className="h-4 w-4"/>
-                    <span className="text-sm">Back to Game</span>
+                    <span className="text-sm">Back to Home</span>
                 </div>
             </Button>
         </div>
