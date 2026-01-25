c8ed97ff49222d3e15212a7eab1898d099d9980e
Vicky Varma <varmasvicky26@gmail.com>
2026-01-25 21:53:22 +0000

    My Bets Passbook Support Funds dar baar is pe clik karu to 10 page banja

--- src/components/bottom-navbar.tsx
+++ src/components/bottom-navbar.tsx
@@ -36,6 +36,7 @@ export function BottomNavbar({ settings }: BottomNavbarProps) {
     setActive(getCurrentActiveItem());
   }, [pathname]);
 
+  const isHomePage = pathname === '/';
 
   return (
     <div className="fixed bottom-0 left-0 right-0 z-50">
@@ -54,6 +55,7 @@ export function BottomNavbar({ settings }: BottomNavbarProps) {
               <Link
                 key={it.id}
                 href={it.path}
+                replace={!isHomePage}
                 onClick={() => setActive(it.id)}
                 aria-current={activeNow ? "page" : undefined}
                 aria-label={it.label}
