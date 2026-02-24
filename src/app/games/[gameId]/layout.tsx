2b9d7729eecd4103cf715f67f50082cfb10dfbef
Vicky Varma <varmasvicky26@gmail.com>
2026-02-24 19:55:49 +0000

    aur smoth karo bohot atak raha hai

--- src/app/games/[gameId]/layout.tsx
+++ src/app/games/[gameId]/layout.tsx
@@ -141,9 +141,9 @@ export default function GameLayout({ children }: { children: React.ReactNode }) 
                         animate={{ x: 0, opacity: 1 }}
                         exit={{ x: -300, opacity: 0 }}
                         transition={{
-                            type: "spring",
-                            stiffness: 180,
-                            damping: 25,
+                            type: "tween",
+                            ease: "easeInOut",
+                            duration: 0.4
                         }}
                     >
                         {children}
