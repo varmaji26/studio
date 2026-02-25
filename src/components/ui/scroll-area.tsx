bf1fa9edf42e9bf740b41831131829fad58062c4
Vicky Varma <varmasvicky26@gmail.com>
2026-02-25 20:29:14 +0000

    ग्लोबल स्टाइल अपडेट: src/app/globals.css में स्क्रॉल बार को पूरी तरह से

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -18,6 +18,7 @@ const ScrollArea = React.forwardRef<
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
       {children}
     </ScrollAreaPrimitive.Viewport>
+    <ScrollBar />
     <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
 ))
